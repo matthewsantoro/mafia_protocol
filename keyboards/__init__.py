329c1f256661ef079bc231114c198c58d78c12da
Matvey Edigaryev <matthewsantoro@protonmail.com>
2022-11-09 16:53:49 +0500

    add new kb for extra point

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,2 +1,2 @@
 from keyboards.client_kb import kb_client
-from keyboards.game_kb import kb_vote, kb_murder, kb_skip
+from keyboards.game_kb import kb_vote, kb_murder, kb_skip, kb_extra_points
--- keyboards/game_kb.py
+++ keyboards/game_kb.py
@@ -1,4 +1,7 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,
+                           KeyboardButton, ReplyKeyboardMarkup,
+                           ReplyKeyboardRemove)
+from answer_text import Emojis
 
 vote_button = KeyboardButton('Никто не покинул')
 kb_vote = ReplyKeyboardMarkup(resize_keyboard=True)
@@ -11,3 +14,35 @@ kb_murder.add(murder_button)
 skip_button = KeyboardButton('Пропустить')
 kb_skip = ReplyKeyboardMarkup(resize_keyboard=True)
 kb_skip.add(skip_button)
+
+kb_extra_points = ReplyKeyboardMarkup(resize_keyboard=True)
+x = [
+    KeyboardButton(Emojis['1']),
+    KeyboardButton(Emojis['2']),
+    KeyboardButton(Emojis['3']),
+    KeyboardButton(Emojis['4']),
+    KeyboardButton(Emojis['5']),
+    KeyboardButton(Emojis['6']),
+    KeyboardButton(Emojis['7']),
+    KeyboardButton(Emojis['8']),
+    KeyboardButton(Emojis['9']),
+    KeyboardButton(Emojis['10']),
+    KeyboardButton(Emojis['ok'])
+]
+kb_extra_points.add(*x)
+
+# 
+# kb_extra_points = InlineKeyboardMarkup(row_width=5)
+# x = [InlineKeyboardButton(text='1️⃣', callback_data="ep_1"),
+#      InlineKeyboardButton(text='2️⃣', callback_data="ep_2"),
+#      InlineKeyboardButton(text='3️⃣', callback_data="ep_3"),
+#      InlineKeyboardButton(text='4️⃣', callback_data="ep_4"),
+#      InlineKeyboardButton(text='5️⃣', callback_data="ep_5"),
+#      InlineKeyboardButton(text='6️⃣', callback_data="ep_6"),
+#      InlineKeyboardButton(text='7️⃣', callback_data="ep_7"),
+#      InlineKeyboardButton(text='8️⃣', callback_data="ep_8"),
+#      InlineKeyboardButton(text='9️⃣', callback_data="ep_9"),
+#      InlineKeyboardButton(text='🔟', callback_data="ep_10"),
+#      InlineKeyboardButton(text='🆗', callback_data="ep_ok"),
+#      ]
+# kb_extra_points.add(*x)
